781b81f82e3732cdfcd38ac4975e239c002d1e40
sebin saji <sebinsajiabraham@gmail.com>
2023-06-17 09:04:39 +0530

    Add files via upload

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
+function Footer() {
+  return (
+    
+  )
+}
+
+export default Footer
